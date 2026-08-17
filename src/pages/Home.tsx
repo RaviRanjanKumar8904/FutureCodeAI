e4c5fedd130b3aca71e1423e020c7693099f2931
FutureCodeAI <admin@futurecode.ai>
2026-08-17 16:29:51 +0530

    revert: restore original clean homepage layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,8 @@ import SEO from '../components/SEO';
 import HeroSection from '../components/home/HeroSection';
 import TrustBar from '../components/home/TrustBar';
 import OfferingsSection from '../components/home/OfferingsSection';
-import FeaturedCourses from '../components/home/FeaturedCourses';
 import TimelineSection from '../components/home/TimelineSection';
-import StatsSection from '../components/home/StatsSection';
-import TestimonialsSection from '../components/home/TestimonialsSection';
 import EnquirySection from '../components/home/EnquirySection';
-import CTABanner from '../components/home/CTABanner';
 
 export default function Home() {
   return (
@@ -22,14 +18,10 @@ export default function Home() {
       
       <main className="w-full">
         <HeroSection />
-        <TrustBar />
         <OfferingsSection />
-        <FeaturedCourses />
         <TimelineSection />
-        <StatsSection />
-        <TestimonialsSection />
         <EnquirySection />
-        <CTABanner />
+        <TrustBar />
       </main>
     </div>
   );
